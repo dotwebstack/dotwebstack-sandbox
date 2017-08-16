5271cfd771d019dc9024d90bc546bc72f054f020
Joost Farla <joost@apiwise.nl>
2017-08-16 18:00:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# DotWebStack Sandbox
+
+A sandbox environment to play with the DotWebStack framework.
+
+## Usage
+
+Put your .ttl configuration files in `src/main/resources/model`.
+
+Put your Open API Specification files in `src/main/resources/openapi`.
+
+To start the application, run:
+
+```
+mvn spring-boot:run
+```
---
+++ src/test/java/org/dotwebstack/sandbox/dotwebstacksandbox/DotwebstackSandboxApplicationTests.java
@@ -0,0 +1,17 @@
+package org.dotwebstack.sandbox.dotwebstacksandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DotwebstackSandboxApplicationTests {
+
+  @Test
+  public void contextLoads() {
+
+  }
+
+}
